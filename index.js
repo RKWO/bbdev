9763a3c9d17fc158efb9d2113b571c1b01e9ec2e
RKWO <68783458+RKWO@users.noreply.github.com>
2023-12-04 22:40:27 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-import React from "react";
-import {NativeModules} from "react-native";
-import CustomBuddyScreen from "./custom_code/CustomBuddyScreen";
-const {RNCustomCode} = NativeModules;
- 
-export const applyCustomCode = externalCodeSetup => {
-  externalCodeSetup.navigationApi.addNavigationRoute(
-"customScreen",
-"CustomBuddyScreen",
-CustomBuddyScreen,
-"Main" // "Auth" | "noAuth" | "Main" | "All"
-  );
+export const applyCustomCode = (externalCodeSetup: any) => {
+	// call custom code api here
 };
-
-externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", () => (
-    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-        <Text>This is my signup screen.</Text>
-    </View>
-));
